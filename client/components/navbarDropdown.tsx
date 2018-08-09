148d7e719d2f45c64094a3860e40ac0cf2b10e1a
Otsuki Hitoshi <hitochan777@gmail.com>
2018-08-09 18:44:29 +0900

    Refactor

--- client/components/navbarDropdown.tsx
+++ client/components/navbarDropdown.tsx
@@ -1,21 +1,15 @@
-import * as React from "react";
+import React from "react";
 
 interface IProps {
   title: string;
   items: string[];
 }
 
-export default class NavbarDropdown extends React.Component<IProps> {
-  public render() {
-    return (
-      <div className="navbar-item has-dropdown is-hoverable">
-        <span className="navbar-link">{this.props.title}</span>
-        <div className="navbar-dropdown is-boxed">
-          {this.props.items.map((item: string) => (
-            <span className="navbar-item">{item}</span>
-          ))}
-        </div>
-      </div>
-    );
-  }
-}
+export default ({ title, items }: IProps) => (
+  <div className="navbar-item has-dropdown is-hoverable">
+    <span className="navbar-link">{title}</span>
+    <div className="navbar-dropdown is-boxed">
+      {items.map(item => <span className="navbar-item">{item}</span>)}
+    </div>
+  </div>
+);
--- client/layouts/main.tsx
+++ client/layouts/main.tsx
@@ -6,9 +6,9 @@ import Header from "../containers/header";
 const style = require("../styles/style.scss");
 
 export default ({ children }) => (
-  <div>
+  <>
     <style dangerouslySetInnerHTML={{ __html: style }} />
     <Header />
     {children}
-  </div>
+  </>
 );
