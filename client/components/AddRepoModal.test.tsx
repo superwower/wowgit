c80e916a6e3b4693784020900a86b642aef594e4
Otsuki Hitoshi <hitochan777@gmail.com>
2018-08-24 00:09:50 +0900

    Add more tests to HOCs of AddRepoModal

--- client/components/AddRepoModal.test.tsx
+++ client/components/AddRepoModal.test.tsx
@@ -4,49 +4,79 @@ import * as TestRenderer from "react-test-renderer";
 
 import {
   addRepoModal as PureAddRepoModal,
+  enhance,
   ImportType,
   IProps
 } from "./AddRepoModal";
 
-let instance: TestRenderer.ReactTestInstance;
+const createProps = (props: { [K in keyof IProps]?: IProps[K] }): IProps => ({
+  activeTab: "REMOTE",
+  addRepo: () => {},
+  closeModal: () => {},
+  isActive: false,
+  name: "",
+  onAddClick: () => {},
+  onCancelClick: () => {},
+  setActiveTab: activeTab => {},
+  setName: name => {},
+  setSrc: src => {},
+  src: "",
+  ...props
+});
 
-const createPureAddRepoModal = (props: { [K in keyof IProps]?: IProps[K] }) => {
-  const mergedProps: IProps = {
-    activeTab: "REMOTE",
-    addRepo: () => {},
-    closeModal: () => {},
-    isActive: false,
-    name: "",
-    onAddClick: () => {},
-    onCancelClick: () => {},
-    setActiveTab: activeTab => {},
-    setName: name => {},
-    setSrc: src => {},
-    src: "",
-    ...props
-  };
-  return <PureAddRepoModal {...mergedProps} />;
-};
+/* tslint:disable-next-line:variable-name*/
+const MockComponent: React.SFC<{}> = () => <div />;
 
 it("calls closeModal when x button is clicked", () => {
   const closeModal = jest.fn();
   const testRenderer = TestRenderer.create(
-    createPureAddRepoModal({ closeModal })
+    <PureAddRepoModal {...createProps({ closeModal })} />
   );
-  instance = testRenderer.root;
+  const instance = testRenderer.root;
   const closeButton = instance.find(el => el.type === "button");
   closeButton.props.onClick();
   expect(closeModal).toHaveBeenCalledTimes(1);
 });
 
-it("clears name and src when Add button is clicked", () => {
-  // TODO: implement
-});
+describe("recompose HOCs", () => {
+  describe("onAddClick", () => {
+    it(`1) adds repo to repo list with the specified name
+        2) clears name and src when Add button is clicked
+        3) closes modal`, () => {
+      const addRepo = jest.fn();
+      const closeModal = jest.fn();
+      /* tslint:disable-next-line:variable-name*/
+      const Enhanced = enhance(MockComponent);
+      const testRenderer = TestRenderer.create(
+        <Enhanced addRepo={addRepo} closeModal={closeModal} />
+      );
+      const instance = testRenderer.root.findByType(MockComponent);
+      instance.props.setName("repo name");
+      instance.props.setSrc("repo src");
+      instance.props.onAddClick();
+      expect(instance.props.name).toBe("");
+      expect(instance.props.src).toBe("");
+      expect(addRepo).toHaveBeenCalledWith({
+        name: "repo name",
+        src: "repo src"
+      });
+      expect(closeModal).toHaveBeenCalledTimes(1);
+    });
+  });
 
-it("clears name and src when Cancel button is clicked", () => {
-  // TODO: implement
-});
-
-it("add name to repository list when Add button is clicked", () => {
-  // TODO: implement
+  describe("onCancelClick", () => {
+    it("clears name and src when Cancel button is clicked", () => {
+      const closeModal = jest.fn();
+      /* tslint:disable-next-line:variable-name*/
+      const Enhanced = enhance(MockComponent);
+      const testRenderer = TestRenderer.create(
+        <Enhanced closeModal={closeModal} />
+      );
+      const instance = testRenderer.root.findByType(MockComponent);
+      instance.props.onCancelClick();
+      expect(instance.props.name).toBe("");
+      expect(instance.props.src).toBe("");
+      expect(closeModal).toHaveBeenCalledTimes(1);
+    });
+  });
 });
