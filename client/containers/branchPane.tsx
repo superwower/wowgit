e8708311bf8e6792ad51e03bb728127b88e1c05f
kut-arika <kut.arika.1114@gmail.com>
2018-09-18 12:54:29 +0900

    impl branchPane view and fix format

--- client/containers/branchPane.tsx
+++ client/containers/branchPane.tsx
@@ -1,25 +1,27 @@
 import * as React from "react";
 import { connect } from "react-redux";
-import { compose, lifecycle, withState, ReactLifeCycleFunctions } from "recompose";
+import { compose, lifecycle, ReactLifeCycleFunctions } from "recompose";
 import { AnyAction, bindActionCreators, Dispatch } from "redux";
 
 import RemoteBranches from "../components/remoteBranches";
-import { IStoreST, remotes } from "../models";
+import { IStoreST, remotesAggs } from "../models";
 import { IRemotesST } from "../models/remotes";
 
-export interface IProps  {
-  remotes: IRemotesST
+export interface IProps {
+  remotes: IRemotesST;
+  fetchRemotes: () => void;
 }
 
-export const branchPane = ({remotes}: IProps) => (
+export const branchPane = ({ remotes, fetchRemotes }: IProps) => (
   <div>
     <div className="message is-light">
       <div className="message-header">Remote</div>
-      <div className="message-body"> 
-      {remotes.items.map((item, index) => (
+      <div className="message-body">
+        {remotes.items.map((item, index) => (
           <RemoteBranches
             remote={item.name}
             branches={item.branches}
+            key={index}
           />
         ))}
       </div>
@@ -29,24 +31,22 @@ export const branchPane = ({remotes}: IProps) => (
 
 const lifeCycleFunctions: ReactLifeCycleFunctions = {
   componentDidMount() {
-    this.props.fetchRemotes()
-    this.setState({remotes: {items: []}});
-  },
+    this.props.fetchRemotes();
+  }
 };
 
 const mapDispatch = (dispatch: Dispatch<AnyAction>) =>
   bindActionCreators(
     {
-      fetchRemotes: remotes.creators.fetchRemotes
+      fetchRemotes: remotesAggs.creators.fetchRemotes
     },
     dispatch
   );
 
 export default compose(
   connect(
-    (store: IStoreST) => ({}),
+    (store: IStoreST) => ({ remotes: store.remotes }),
     (dispatch: Dispatch<AnyAction>) => mapDispatch(dispatch)
   ),
-  withState("remotes", "setActiveTab", {items: []}),
-  lifecycle(lifeCycleFunctions),
+  lifecycle(lifeCycleFunctions)
 )(branchPane);
